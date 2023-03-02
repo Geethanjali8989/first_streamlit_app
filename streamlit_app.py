508a85118dd627c82a508803c810ccc1ba4b2404
Geethanjali Gudimetla <50764449+Geethanjali8989@users.noreply.github.com>
2023-03-02 22:38:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit
 streamlit.title ("My parents new healthy diner")
 
-streamlit.header ("Breakfast menu")
-streamlit.text('IDLY')
-streamlit.text('DOSA')
-streamlit.text('UPMA')
+streamlit.header ("😍Breakfast menu😍")
+streamlit.text('IDLY😎')
+streamlit.text('DOSA😉')
+streamlit.text('UPMA🍒🍊')
 
 
 streamlit.header('Breakfast Menu')
@@ -17,4 +17,11 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
+
 streamlit.dataframe(my_fruit_list)
