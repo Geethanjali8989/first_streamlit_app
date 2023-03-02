6fa60916e18f6d4ebc1afe03bb7dd96f73f42ade
Geethanjali Gudimetla <50764449+Geethanjali8989@users.noreply.github.com>
2023-03-02 13:41:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 streamlit.title ("My parents new healthy diner")
 
 streamlit.header ("Breakfast menu")
-streamlit.text('IDLY'
+streamlit.text('IDLY')
 streamlit.text('DOSA')
 streamlit.text('UPMA')
