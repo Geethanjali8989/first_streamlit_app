cbed4c4b25f702e1002e0d577b6276eec2cb6de2
Geethanjali Gudimetla <50764449+Geethanjali8989@users.noreply.github.com>
2023-03-12 12:16:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 #new section to display fruity wise api resdponse
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','apple')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Apple')
 streamlit.write('The user entered ', fruit_choice)
 import requests
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
